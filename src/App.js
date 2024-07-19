955f95b457d4b4495626788050d3f7151ea94ac7
Carooh55 <carolinemakhadzi40@gmail.com>
2024-07-19 20:09:51 +0200

    added the url github

--- src/App.js
+++ src/App.js
@@ -3,5 +3,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project is coded by{" "}
+        <a href="https://github.com/Carooh55" target="blank">
+          Makhadzi Ramadi
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/Carooh55/search-engine" target="blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
