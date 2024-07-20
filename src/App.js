665b713d555a9239b7808e5395847b2bd1004644
Carooh55 <carolinemakhadzi40@gmail.com>
2024-07-20 12:22:14 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from "react";
-
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is coded by{" "}
-        <a href="https://github.com/Carooh55" target="blank">
-          Makhadzi Ramadi
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/Carooh55/search-engine" target="blank">
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is coded by{" "}
+          <a href="https://github.com/Carooh55" target="blank">
+            Makhadzi Ramadi
+          </a>{" "}
+          and is{" "}
+          <a href="https://github.com/Carooh55/search-engine" target="blank">
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
