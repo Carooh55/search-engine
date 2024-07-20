bd2a9ec623bc352fd73f657b95f2db956384871b
Carooh55 <carolinemakhadzi40@gmail.com>
2024-07-20 14:01:31 +0200

    added css

--- src/App.js
+++ src/App.js
@@ -6,15 +6,32 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather />
+        <Weather defaultCity="New York" />
+
         <footer>
           This project is coded by{" "}
-          <a href="https://github.com/Carooh55" target="blank">
+          <a
+            href="https://github.com/Carooh55"
+            target="blank"
+            rel="noopener noreferrer"
+          >
             Makhadzi Ramadi
           </a>{" "}
           and is{" "}
-          <a href="https://github.com/Carooh55/search-engine" target="blank">
-            open-sourced on GitHub.
+          <a
+            href="https://github.com/Carooh55/search-engine"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-search-enginee.netlify.app/"
+            target="blank"
+            rel="noopener noreferrer"
+          >
+            hosted on Netlify.
           </a>
         </footer>
       </div>
